af5d4d8280e9f8b71f34a9d3b887600ea6948592
scott <scott@adligo.com>
2012-01-13 01:11:25 +0000

    added javadoc

--- src/org/adligo/cl/args/ArgParser.java
+++ src/org/adligo/cl/args/ArgParser.java
@@ -9,6 +9,19 @@ import org.adligo.i.log.client.LogFactory;
 public class ArgParser {
 	private static final Log log = LogFactory.getLog(ArgParser.class);
 	
+	/**
+	 * this parses strings for command line input
+	 * ie
+	 * yourProgram.jar -help -threads='123' -mojo=0
+	 * it returns empty strings for for items with out quotes
+	 * so 
+	 * -help would return ""
+	 * -threads would return "123"
+	 * and -mojo would return ""
+	 * 
+	 * @param valueObject
+	 * @return
+	 */
 	public static Map<String,String> parse(String[] valueObject) {
 		Map<String,String> toRet = new HashMap<String,String>();
 		for (int i = 0; i < valueObject.length; i++) {
